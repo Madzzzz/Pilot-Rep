036fd38daeec75d4c6cde2a6e8ca8771fb2b9b17
Mads Hellenes <madpiz@hotmail.com>
2016-01-09 14:57:32 +0100

    FEEL THE BERN
    power wheel placeholder detects wich placeholder u use, long live the if
    settings

--- PowerWheel.cs
+++ PowerWheel.cs
@@ -6,29 +6,52 @@ public class PowerWheel : MonoBehaviour {
     void Start() {
         gameObject.GetComponent<Renderer>().enabled = false;
     }
+
+    void detectWhichPower()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Rage")
+            {
+                Debug.Log("FEEL THE BERN");
+            }
+
+            if (hit.transform.name == "Fear")
+            {
+                Debug.Log("DEUS VULT");
+            }
+
+            if (hit.transform.name == "Happy")
+            {
+                Debug.Log("CANT STUMP THE TRUMP");
+            }
+
+            if (hit.transform.name == "Depression")
+            {
+                Debug.Log("ME IRL");
+            }
+
+            if (hit.transform.name == "Regret")
+            {
+                Debug.Log("NO REGRATS");
+            }
+        }
+
+        
+    }
 	
 	void Update () {
 
-        if (Input.GetMouseButton (1)) 
+        if (Input.GetMouseButton(1))
             gameObject.GetComponent<Renderer>().enabled = true;
         else
             gameObject.GetComponent<Renderer>().enabled = false;
 
-        /*
-        
-        if (gameObject.tag == ("Happy"))
-            Debug.Log("HAPPY POWER ACTIVATE");
-        if (gameObject.tag == ("Depression"))
-            Debug.Log("DEPRESSION POWER ACTIVATE");
-        if (gameObject.tag == ("Fear"))
-            Debug.Log("FEAR POWER ACTIVATE");
-        if (gameObject.tag == ("Rage"))
-            Debug.Log("RAGE POWER ACTIVATE");
-        if (gameObject.tag == ("Regret"))
-            Debug.Log("REGRET POWER ACTIVATE");
-
-        */
-
-
+        if(Input.GetMouseButtonUp(1))
+        {
+            detectWhichPower();
+        }
     }
 }
